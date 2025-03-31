d46b0f68ccee529d30dd5aad042c2a4fef52bba0
canpok1 <6isstrong@gmail.com>
2025-03-31 15:35:56 +0000

    レビュー対応

--- frontend/src/planning-poker/TopPage.tsx
+++ frontend/src/planning-poker/TopPage.tsx
@@ -2,18 +2,18 @@ import { LogIn, Users } from "lucide-react";
 
 function TopPage() {
     return (
-        <div className="max-w-200 mx-auto py-25 text-center">
+        <div className="max-w-2xl mx-auto py-25 text-center">
             <div className="mb-5">
                 <h1 className="text-3xl font-bold">プランニングポーカー</h1>
             </div>
             <div className="mb-5">
-                <a href="./sessions/create" className="btn min-w-5/6">
+                <a href="./sessions/create" className="btn min-w-5/6" aria-label="セッションを作成">
                     <LogIn />セッションを作成
                 </a>
             </div>
             <div className="justify-center">
-                <a href="./sessions/join" className="btn min-w-5/6">
-                    <Users />セッションを作成
+                <a href="./sessions/join" className="btn min-w-5/6" aria-label="セッションに参加">
+                    <Users />セッションに参加
                 </a>
             </div>
         </div>
