251d0c3a54e8608db768a349c2be4c45a5cb05db
canpok1 <6isstrong@gmail.com>
2025-03-31 15:53:36 +0000

    レビュー対応

--- frontend/src/planning-poker/TopPage.tsx
+++ frontend/src/planning-poker/TopPage.tsx
@@ -8,12 +8,12 @@ function TopPage() {
                 <h1 className="text-3xl font-bold">プランニングポーカー</h1>
             </div>
             <div className="mb-5">
-                <Link to="./sessions/create" className="btn min-w-5/6">
+                <Link to="./sessions/create" className="btn min-w-5/6" aria-label="セッションを作成">
                     <LogIn />セッションを作成
                 </Link>
             </div>
             <div className="justify-center">
-                <Link to="./sessions/join" className="btn min-w-5/6">
+                <Link to="./sessions/join" className="btn min-w-5/6" aria-label="セッションに参加">
                     <Users />セッションに参加
                 </Link>
             </div>
