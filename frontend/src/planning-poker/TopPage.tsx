db14c4a048a6b5c830c785e60839138b214752f4
canpok1 <canpok1@users.noreply.github.com>
2025-04-01 00:37:27 +0900

    daisyUIとlucide-reactを導入してプランニングポーカーのトップ画面を作成 (#24)
    * daisyUIとlucide-reactを導入
    
    * プランニングポーカーのトップ画面をスタイリング
    
    * レビュー対応

--- frontend/src/planning-poker/TopPage.tsx
+++ frontend/src/planning-poker/TopPage.tsx
@@ -1,5 +1,23 @@
+import { LogIn, Users } from "lucide-react";
+
 function TopPage() {
-    return <h1>プランニングポーカートップ画面</h1>
+    return (
+        <div className="max-w-2xl mx-auto py-25 text-center">
+            <div className="mb-5">
+                <h1 className="text-3xl font-bold">プランニングポーカー</h1>
+            </div>
+            <div className="mb-5">
+                <a href="./sessions/create" className="btn min-w-5/6" aria-label="セッションを作成">
+                    <LogIn />セッションを作成
+                </a>
+            </div>
+            <div className="justify-center">
+                <a href="./sessions/join" className="btn min-w-5/6" aria-label="セッションに参加">
+                    <Users />セッションに参加
+                </a>
+            </div>
+        </div>
+    )
 }
 
 export default TopPage;
