008a59f92a2e978ec0fed1309cfdf8cd33d4f575
canpok1 <6isstrong@gmail.com>
2025-04-05 15:31:19 +0000

    投票結果を公開時と未公開時とで表示を変えるよう修正

--- frontend/src/planning-poker/components/RoundSummary.tsx
+++ frontend/src/planning-poker/components/RoundSummary.tsx
@@ -1,21 +1,20 @@
 import type { RoundParticipant } from "../types/Participant";
+import VoteResult from "./VoteResult";
 
 export type RoundSummaryProps = {
   round: number;
   participants: RoundParticipant[];
+  revealed: boolean;
 };
 
-function RoundSummary({ round, participants }: RoundSummaryProps) {
+function RoundSummary({ round, participants, revealed }: RoundSummaryProps) {
   return (
     <div className="card mx-auto mb-5 max-w-2xl shadow-sm">
       <div className="card-body bg-neutral-content text-left">
         <h2 className="card-title">ラウンド{round}：投票結果</h2>
         <div className="grid grid-cols-5 gap-4">
           {participants.map((p) => (
-            <div key={p.id} className="stat place-items-center">
-              <div className="stat-title">{p.name}</div>
-              <div className="stat-value">{p.vote}</div>
-            </div>
+            <VoteResult key={p.id} participant={p} revealed={revealed} />
           ))}
         </div>
       </div>
