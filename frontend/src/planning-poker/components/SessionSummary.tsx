2836ddc70685055e5ed511fa7be21d0b638a3485
canpok1 <6isstrong@gmail.com>
2025-04-12 14:36:41 +0000

    不要なスペースを削除

--- frontend/src/planning-poker/components/SessionSummary.tsx
+++ frontend/src/planning-poker/components/SessionSummary.tsx
@@ -31,7 +31,7 @@ function SessionSummary({ session }: { session: Session }) {
     <div className="card mx-auto mb-5 max-w-2xl shadow-sm">
       <div className="card-body bg-neutral-content text-left">
         <h2 className="card-title">セッション名: {session.name}</h2>
-        <p className="mt-4">参加者: {names.join(", ")}</p>{" "}
+        <p className="mt-4">参加者: {names.join(", ")}</p>
         <div>
           <button
             type="button"
