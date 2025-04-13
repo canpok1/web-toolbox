139b9c6b512144b2a2e76a6399882a7aff31b9bc
canpok1 <6isstrong@gmail.com>
2025-04-13 15:32:17 +0000

    各種情報の組立をuseSessionに集約

--- frontend/src/planning-poker/SessionPage.tsx
+++ frontend/src/planning-poker/SessionPage.tsx
@@ -1,84 +1,32 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
+import Alert from "./components/Alert";
 import FibonacciVotePanel from "./components/FibonacciVotePanel";
 import HostPanel, { type HostPanelEvent } from "./components/HostPanel";
 import RoundSummary from "./components/RoundSummary";
 import SessionSummary from "./components/SessionSummary";
-import useRound from "./hooks/useRound";
 import useSession from "./hooks/useSession";
-import type { RoundParticipant } from "./types/Participant";
+import { ExtractErrorMessage } from "./utils/error";
 
 function SessionPage() {
-  const { sessionId } = useParams<{ sessionId: string }>();
-  const { session, fetch: fetchSession } = useSession();
-  const { round, fetch: fetchRound } = useRound();
-
   const [searchParams] = useSearchParams();
-  const participandId = searchParams.get("id") ?? "";
-  const showHostPanel = session && participandId === session?.hostId;
 
-  const [voteOption, setVoteOption] = useState<string | null>(null);
-  const [roundParticipants, setRoundParticipants] = useState<
-    RoundParticipant[]
-  >([]);
+  const { sessionId = "" } = useParams<{ sessionId: string }>();
+  const participandId = searchParams.get("id") ?? "";
+
+  const { session, round, myVote, error, reload } = useSession(
+    sessionId,
+    participandId,
+  );
+  const errorMessages = error ? [ExtractErrorMessage(error)] : [];
+
+  const showHostPanel = session && participandId === session?.hostId;
 
   const intervalIdRef = useRef<NodeJS.Timeout | null>(null); // setIntervalのIDを保持
 
-  // セッション情報を更新
-  useEffect(() => {
-    if (sessionId) {
-      (async () => {
-        await fetchSession(sessionId);
-      })();
-    }
-  }, [sessionId, fetchSession]);
-
-  // ラウンド情報を更新
-  useEffect(() => {
-    const roundId = session?.currentRoundId;
-    if (roundId) {
-      (async () => {
-        await fetchRound(roundId, participandId);
-      })();
-    }
-  }, [session, participandId, fetchRound]);
-
-  // 自分の投票情報を更新
-  useEffect(() => {
-    const myVote = round?.votes.find(
-      (vote) => vote.participantId === participandId,
-    );
-    setVoteOption(myVote?.vote ?? null);
-  }, [round, participandId]);
-
-  // ラウンド参加者情報を更新
-  useEffect(() => {
-    if (!session || !round) {
-      return;
-    }
-    const participants: RoundParticipant[] = [];
-    for (const participant of session.participants) {
-      const isVoted = round.votes.some(
-        (vote) => vote.participantId === participant.id,
-      );
-      const vote =
-        round.votes.find((vote) => vote.participantId === participant.id)
-          ?.vote ?? null;
-
-      participants.push({
-        id: participant.id,
-        name: participant.name,
-        isVoted,
-        vote,
-      });
-    }
-
-    setRoundParticipants(participants);
-  }, [session, round]);
-
   // 定期更新
   useEffect(() => {
-    intervalIdRef.current = setInterval(update, 5000);
+    intervalIdRef.current = setInterval(reload, 5000);
     return () => {
       if (intervalIdRef.current) {
         console.log(
@@ -89,20 +37,7 @@ function SessionPage() {
         intervalIdRef.current = null;
       }
     };
-  }, []);
-
-  const update = async () => {
-    if (sessionId) {
-      await fetchSession(sessionId);
-    }
-    if (round?.roundId) {
-      await fetchRound(round.roundId, participandId);
-    }
-  };
-
-  const handleVote = (option: string) => {
-    setVoteOption(option);
-  };
+  }, [reload]);
 
   return (
     <section className="mx-auto max-w-2xl px-5 py-25 text-center">
@@ -110,12 +45,15 @@ function SessionPage() {
         <h1 className="mb-5 font-bold text-3xl">プランニングポーカー</h1>
         {session && <SessionSummary session={session} />}
 
+        <Alert messages={errorMessages} />
+
         {showHostPanel && (
           <HostPanel
             session={session}
             round={round}
             onClick={(event: HostPanelEvent): void => {
               console.log("clicked HostPanel, event:", event);
+              reload();
             }}
             onError={(event: HostPanelEvent, error: unknown): void => {
               console.log("error on HostPanel, event:", event, "error:", error);
@@ -127,14 +65,16 @@ function SessionPage() {
           <FibonacciVotePanel
             roundId={round.roundId}
             participantId={participandId}
-            votedOption={voteOption}
-            onAfterVote={handleVote}
+            votedOption={myVote?.vote ?? null}
+            onAfterVote={() => {
+              reload();
+            }}
           />
         )}
 
         {round && (
           <RoundSummary
-            participants={roundParticipants}
+            participants={round.participants}
             revealed={round?.status === "revealed"}
           />
         )}
--- frontend/src/planning-poker/hooks/useSession.ts
+++ frontend/src/planning-poker/hooks/useSession.ts
@@ -1,46 +1,154 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ApiClient } from "../../api/ApiClient";
+import type { RoundParticipant } from "../types/Participant";
+import type { Round } from "../types/Round";
 import { type Session, isSessionStatus } from "../types/Session";
+import type { Vote } from "../types/Vote";
 
 export type ReturnValue = {
   session: Session | null;
-  fetch: (sessionId: string) => Promise<void>;
+  round: Round | null;
+  myVote: Vote | null;
+  error: unknown | null;
+  reload: () => Promise<void>;
 };
 
-function useSession(): ReturnValue {
+export default function useSession(
+  sessionId: string,
+  participantId: string,
+): ReturnValue {
   const [session, setSession] = useState<Session | null>(null);
+  const [round, setRound] = useState<Round | null>(null);
+  const [myVote, setMyVote] = useState<Vote | null>(null);
+  const [error, setError] = useState<unknown | null>(null);
 
-  const fetch = useCallback(async (sessionId: string) => {
-    const apiClient = new ApiClient();
-    const response = await apiClient.fetchSession(sessionId);
-    if (response.session) {
-      if (!isSessionStatus(response.session.status)) {
-        throw new Error(
-          `Invalid session status, status=${response.session.status}`,
-        );
+  useEffect(() => {
+    (async () => {
+      await reload();
+    })();
+  }, []);
+
+  const reload = useCallback(async () => {
+    try {
+      const apiClient = new ApiClient();
+
+      const session = await fetchSession(apiClient, sessionId);
+      setSession(session);
+
+      if (!session) {
+        setRound(null);
+        setMyVote(null);
+        setError(null);
+        return;
       }
 
-      const participants = response.session.participants.map((participant) => {
-        return {
-          id: participant.participantId,
-          name: participant.name,
-        };
-      });
-      setSession({
-        sessionId: response.session.sessionId,
-        sessionName: response.session.sessionName,
-        participants,
-        currentRoundId: response.session.currentRoundId,
-        hostId: response.session.hostId,
-        status: response.session.status,
-      });
+      const round = await fetchRound(apiClient, session, participantId);
+      setRound(round);
+
+      if (!round) {
+        setMyVote(null);
+        setError(null);
+        return;
+      }
+
+      setMyVote(findMyVote(participantId, round));
+      setError(null);
+    } catch (error) {
+      setError(error);
     }
-  }, []);
+  }, [sessionId, participantId]);
+
+  const fetchSession = async (
+    apiClient: ApiClient,
+    sessionId: string,
+  ): Promise<Session | null> => {
+    const response = await apiClient.fetchSession(sessionId);
+    if (!response.session) {
+      return null;
+    }
+    if (!isSessionStatus(response.session.status)) {
+      throw new Error(
+        `Invalid session status, status=${response.session.status}`,
+      );
+    }
+
+    const participants = response.session.participants.map((participant) => {
+      return {
+        id: participant.participantId,
+        name: participant.name,
+      };
+    });
+
+    return {
+      sessionId: response.session.sessionId,
+      sessionName: response.session.sessionName,
+      participants,
+      currentRoundId: response.session.currentRoundId,
+      hostId: response.session.hostId,
+      status: response.session.status,
+    };
+  };
+
+  const fetchRound = async (
+    apiClient: ApiClient,
+    session: Session,
+    participantId: string,
+  ): Promise<Round | null> => {
+    if (!session.currentRoundId) {
+      return null;
+    }
+    const response = await apiClient.fetchRound(
+      session.currentRoundId,
+      participantId,
+    );
+    if (!response.round) {
+      return null;
+    }
+    const round = response.round;
+
+    const votes = round.votes.map((vote) => {
+      return {
+        participantId: vote.participantId,
+        vote: vote.value ?? null,
+      };
+    });
+    const participants: RoundParticipant[] = session.participants.map(
+      (participant) => {
+        const isVoted = round.votes.some(
+          (vote) => vote.participantId === participant.id,
+        );
+        const vote = round.votes.find(
+          (vote) => vote.participantId === participant.id,
+        );
+
+        return {
+          id: participant.id,
+          name: participant.name,
+          isVoted,
+          vote: vote?.value ?? null,
+        };
+      },
+    );
+
+    return {
+      ...response.round,
+      votes,
+      participants,
+    };
+  };
+
+  const findMyVote = (participantId: string, round: Round): Vote | null => {
+    const myVote = round.votes.find(
+      (vote) => vote.participantId === participantId,
+    );
+    return myVote ?? null;
+  };
 
   return {
     session,
-    fetch,
+    round,
+    myVote,
+    error,
+    reload,
   };
 }
-
-export default useSession;
