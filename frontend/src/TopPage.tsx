069753cc8143179e503a2686a782e05e7612ea91
canpok1 <6isstrong@gmail.com>
2025-05-17 05:28:39 +0000

    トークルーレットへのリンクを追加

--- frontend/src/TopPage.tsx
+++ frontend/src/TopPage.tsx
@@ -4,12 +4,18 @@ const TopPage = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
       <h1 className="mb-8 font-bold text-4xl">Web Toolbox</h1>
-      <div className="space-y-4">
+      <div className="flex space-x-4">
         <Link
           to="/planning-poker"
           className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
         >
           プランニングポーカー
+        </Link>
+        <Link
+          to="/talk-roulette"
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        >
+          トークルーレット
         </Link>
       </div>
     </div>
