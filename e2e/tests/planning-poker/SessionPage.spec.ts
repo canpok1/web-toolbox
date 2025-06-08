227787d9edc1dca3d04c77d9eec00a1b0592b871
canpok1 <6isstrong@gmail.com>
2025-06-08 14:02:13 +0000

    参加者が複数のe2eテストをコメント解除

--- e2e/tests/planning-poker/SessionPage.spec.ts
+++ e2e/tests/planning-poker/SessionPage.spec.ts
@@ -7,6 +7,7 @@ test.describe("セッション画面", () => {
     await page.goto("/planning-poker/sessions/create");
     await page.getByLabel("あなたの名前").fill(hostUserName);
     await page.getByRole("button", { name: "セッションを作成" }).click();
+    await page.waitForEvent("websocket");
   });
 
   test.describe("参加者一覧と招待リンク", () => {
@@ -38,109 +39,108 @@ test.describe("セッション画面", () => {
       });
     });
 
-    // TODO 成功するように実装する
-    // test.describe("参加者が複数", () => {
-    //   const participantUserName = "参加者ユーザー";
-    //   test("表示内容が正しいこと", async ({ page: hostPage }) => {
-    //     // 招待URLを取得
-    //     await hostPage
-    //       .getByRole("button", { name: "招待URL/QRコード" })
-    //       .click();
-    //     const hostInviteLink = hostPage.locator(
-    //       'a[href*="/planning-poker/sessions/join?id="]',
-    //     );
-    //     await expect(hostInviteLink).toBeVisible();
-    //     const hostInviteLinkValue = await hostInviteLink.getAttribute("href");
-    //     let sessionId = "";
-    //     if (hostInviteLinkValue) {
-    //       sessionId = hostInviteLinkValue.split("id=")[1];
-    //     }
+    test.describe("参加者が複数", () => {
+      const participantUserName = "参加者ユーザー";
+      test("表示内容が正しいこと", async ({ page: hostPage }) => {
+        // 招待URLを取得
+        await hostPage
+          .getByRole("button", { name: "招待URL/QRコード" })
+          .click();
+        const hostInviteLink = hostPage.locator(
+          'a[href*="/planning-poker/sessions/join?id="]',
+        );
+        await expect(hostInviteLink).toBeVisible();
+        const hostInviteLinkValue = await hostInviteLink.getAttribute("href");
+        let sessionId = "";
+        if (hostInviteLinkValue) {
+          sessionId = hostInviteLinkValue.split("id=")[1];
+        }
 
-    //     // 参加者としてセッションに参加
-    //     const participantPage = await hostPage.context().newPage();
-    //     await participantPage.goto(
-    //       `/planning-poker/sessions/join?id=${sessionId}`,
-    //     );
-    //     await participantPage
-    //       .getByLabel("あなたの名前")
-    //       .fill(participantUserName);
-    //     await participantPage
-    //       .getByRole("button", { name: "セッションに参加" })
-    //       .click();
+        // 参加者としてセッションに参加
+        const participantPage = await hostPage.context().newPage();
+        await participantPage.goto(
+          `/planning-poker/sessions/join?id=${sessionId}`,
+        );
+        await participantPage
+          .getByLabel("あなたの名前")
+          .fill(participantUserName);
+        await participantPage
+          .getByRole("button", { name: "セッションに参加" })
+          .click();
 
-    //     // 参加者ユーザー画面に自分の名前が表示されるか確認
-    //     await expect(
-    //       participantPage.getByText(`あなたの名前: ${participantUserName}`),
-    //     ).toBeVisible();
-    //     // 参加者ユーザー画面に参加者一覧が表示されるか確認
-    //     await expect(
-    //       participantPage.getByText(/参加者 \(2名\):\s*/),
-    //     ).toBeVisible();
-    //     await expect(
-    //       participantPage.getByText(participantUserName, { exact: true }),
-    //     ).toBeVisible();
-    //     await expect(
-    //       participantPage.getByText(hostUserName, { exact: true }),
-    //     ).toBeVisible();
-    //     // 参加者ユーザー画面で招待リンクをコピーできることを確認
-    //     await expect(
-    //       participantPage.getByRole("button", {
-    //         name: "参加ページのURLをコピー",
-    //       }),
-    //     ).toBeVisible();
-    //     // 参加者ユーザー画面の招待リンクが正しいことを確認
-    //     await participantPage
-    //       .getByRole("button", { name: "招待URL/QRコード" })
-    //       .click();
-    //     const participantInviteLink = participantPage.locator(
-    //       'a[href*="/planning-poker/sessions/join?id="]',
-    //     );
-    //     await expect(participantInviteLink).toBeVisible();
-    //     const participantInviteLinkValue =
-    //       await participantInviteLink.getAttribute("href");
-    //     await expect(participantInviteLinkValue).toMatch(
-    //       /planning-poker\/sessions\/join\?id=.*/,
-    //     );
+        // 参加者ユーザー画面に自分の名前が表示されるか確認
+        await expect(
+          participantPage.getByText(`あなたの名前: ${participantUserName}`),
+        ).toBeVisible();
+        // 参加者ユーザー画面に参加者一覧が表示されるか確認
+        await expect(
+          participantPage.getByText(/参加者 \(2名\):\s*/),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByText(participantUserName, { exact: true }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByText(hostUserName, { exact: true }),
+        ).toBeVisible();
+        // 参加者ユーザー画面で招待リンクをコピーできることを確認
+        await expect(
+          participantPage.getByRole("button", {
+            name: "参加ページのURLをコピー",
+          }),
+        ).toBeVisible();
+        // 参加者ユーザー画面の招待リンクが正しいことを確認
+        await participantPage
+          .getByRole("button", { name: "招待URL/QRコード" })
+          .click();
+        const participantInviteLink = participantPage.locator(
+          'a[href*="/planning-poker/sessions/join?id="]',
+        );
+        await expect(participantInviteLink).toBeVisible();
+        const participantInviteLinkValue =
+          await participantInviteLink.getAttribute("href");
+        await expect(participantInviteLinkValue).toMatch(
+          /planning-poker\/sessions\/join\?id=.*/,
+        );
 
-    //     await hostPage.bringToFront();
+        await hostPage.bringToFront();
 
-    //     // ホストユーザー画面に自分の名前が表示されるか確認
-    //     await expect(
-    //       hostPage.getByText(`あなたの名前: ${hostUserName}`),
-    //     ).toBeVisible();
-    //     // ホストユーザー画面に参加者一覧が表示されるか確認
-    //     await hostPage.waitForTimeout(500); // 0.5秒待機
-    //     await expect(hostPage.getByText(/参加者 \(2名\):\s*/)).toBeVisible();
-    //     await expect(
-    //       hostPage.getByText(hostUserName, { exact: true }),
-    //     ).toBeVisible();
-    //     await expect(
-    //       hostPage.getByText(participantUserName, { exact: true }),
-    //     ).toBeVisible();
-    //     // ホストユーザー画面で招待リンクをコピーできることを確認
-    //     await expect(
-    //       hostPage.getByRole("button", { name: "参加ページのURLをコピー" }),
-    //     ).toBeVisible();
-    //     // ホストユーザー画面の招待リンクが正しいことを確認
-    //     await hostPage
-    //       .getByRole("button", { name: "招待URL/QRコード" })
-    //       .click();
-    //     try {
-    //       await expect(
-    //         hostInviteLink,
-    //         "ホストの招待リンクが表示されること",
-    //       ).toBeVisible({ timeout: 5000 });
-    //       if (hostInviteLinkValue) {
-    //         await expect(
-    //           hostInviteLinkValue,
-    //           "ホストの招待リンクが正しいこと",
-    //         ).toMatch(/planning-poker\/sessions\/join\?id=.*/);
-    //       }
-    //     } catch (e) {
-    //       console.error("hostInviteLink is not visible", e);
-    //     }
-    //   });
-    // });
+        // ホストユーザー画面に自分の名前が表示されるか確認
+        await expect(
+          hostPage.getByText(`あなたの名前: ${hostUserName}`),
+        ).toBeVisible();
+        // ホストユーザー画面に参加者一覧が表示されるか確認
+        await hostPage.waitForTimeout(500); // 0.5秒待機
+        await expect(hostPage.getByText(/参加者 \(2名\):\s*/)).toBeVisible();
+        await expect(
+          hostPage.getByText(hostUserName, { exact: true }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByText(participantUserName, { exact: true }),
+        ).toBeVisible();
+        // ホストユーザー画面で招待リンクをコピーできることを確認
+        await expect(
+          hostPage.getByRole("button", { name: "参加ページのURLをコピー" }),
+        ).toBeVisible();
+        // ホストユーザー画面の招待リンクが正しいことを確認
+        await hostPage
+          .getByRole("button", { name: "招待URL/QRコード" })
+          .click();
+        try {
+          await expect(
+            hostInviteLink,
+            "ホストの招待リンクが表示されること",
+          ).toBeVisible({ timeout: 5000 });
+          if (hostInviteLinkValue) {
+            await expect(
+              hostInviteLinkValue,
+              "ホストの招待リンクが正しいこと",
+            ).toMatch(/planning-poker\/sessions\/join\?id=.*/);
+          }
+        } catch (e) {
+          console.error("hostInviteLink is not visible", e);
+        }
+      });
+    });
   });
 
   test.describe("ホスト用ボタンと投票ボタンと投票結果", () => {
