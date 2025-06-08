178fd815b3476f35ac4f863c4ceb537b96c859ec
canpok1 <6isstrong@gmail.com>
2025-06-08 05:28:10 +0000

    e2eテスト失敗時に結果をブラウザで表示しないよう修正

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['dot'],
-    ['html', { host: '0.0.0.0', open: process.env.CI ? 'never' : 'on-failure' }]
+    ['html', { host: '0.0.0.0', open: 'never' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
