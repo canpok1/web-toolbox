ad8d73181bd04d95c5b9b421e44c3c6cf786de5e
canpok1 <6isstrong@gmail.com>
2025-06-08 14:54:18 +0000

    プランニングポーカーの投票のe2eテストを追加

--- e2e/tests/planning-poker/SessionPage.spec.ts
+++ e2e/tests/planning-poker/SessionPage.spec.ts
@@ -3,14 +3,14 @@ import { expect, test } from "@playwright/test";
 test.describe("セッション画面", () => {
   const hostUserName = "ホストユーザー";
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/planning-poker/sessions/create");
-    await page.getByLabel("あなたの名前").fill(hostUserName);
-    await page.getByRole("button", { name: "セッションを作成" }).click();
-    await page.waitForEvent("websocket");
-  });
-
   test.describe("参加者一覧と招待リンク", () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/planning-poker/sessions/create");
+      await page.getByLabel("あなたの名前").fill(hostUserName);
+      await page.getByRole("button", { name: "セッションを作成" }).click();
+      await page.waitForEvent("websocket");
+    });
+
     test.describe("ホストのみ", () => {
       test("表示内容が正しいこと", async ({ page: hostPage }) => {
         await expect(
@@ -109,7 +109,6 @@ test.describe("セッション画面", () => {
           hostPage.getByText(`あなたの名前: ${hostUserName}`),
         ).toBeVisible();
         // ホストユーザー画面に参加者一覧が表示されるか確認
-        await hostPage.waitForTimeout(500); // 0.5秒待機
         await expect(hostPage.getByText(/参加者 \(2名\):\s*/)).toBeVisible();
         await expect(
           hostPage.getByText(hostUserName, { exact: true }),
@@ -125,19 +124,15 @@ test.describe("セッション画面", () => {
         await hostPage
           .getByRole("button", { name: "招待URL/QRコード" })
           .click();
-        try {
+        await expect(
+          hostInviteLink,
+          "ホストの招待リンクが表示されること",
+        ).toBeVisible();
+        if (hostInviteLinkValue) {
           await expect(
-            hostInviteLink,
-            "ホストの招待リンクが表示されること",
-          ).toBeVisible({ timeout: 5000 });
-          if (hostInviteLinkValue) {
-            await expect(
-              hostInviteLinkValue,
-              "ホストの招待リンクが正しいこと",
-            ).toMatch(/planning-poker\/sessions\/join\?id=.*/);
-          }
-        } catch (e) {
-          console.error("hostInviteLink is not visible", e);
+            hostInviteLinkValue,
+            "ホストの招待リンクが正しいこと",
+          ).toMatch(/planning-poker\/sessions\/join\?id=.*/);
         }
       });
     });
@@ -145,49 +140,234 @@ test.describe("セッション画面", () => {
 
   test.describe("ホスト用ボタンと投票ボタンと投票結果", () => {
     test.describe("フィボナッチ", () => {
-      test("投票開始→投票→投票公開→投票開始", async () => {
+      test.beforeEach(async ({ page }) => {
+        await page.goto("/planning-poker/sessions/create");
+        await page.getByLabel("あなたの名前").fill(hostUserName);
+        await page.getByRole("button", { name: "セッションを作成" }).click();
+        await page.waitForEvent("websocket");
+      });
+
+      test("投票開始→投票→投票公開→投票開始", async ({ page: hostPage }) => {
+        // 招待URLを取得
+        await hostPage
+          .getByRole("button", { name: "招待URL/QRコード" })
+          .click();
+        const hostInviteLink = hostPage.locator(
+          'a[href*="/planning-poker/sessions/join?id="]',
+        );
+        const hostInviteLinkValue = await hostInviteLink.getAttribute("href");
+        let sessionId = "";
+        if (hostInviteLinkValue) {
+          sessionId = hostInviteLinkValue.split("id=")[1];
+        }
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票開始ボタンが表示されないことを確認
-        // TODO ホストユーザー画面に投票開始ボタンが表示されることを確認
-        // TODO ホストが投票を開始する
+        const participantUserName = "参加者ユーザー";
+        const participantPage = await hostPage.context().newPage();
+        await participantPage.goto(
+          `/planning-poker/sessions/join?id=${sessionId}`,
+        );
+        await participantPage
+          .getByLabel("あなたの名前")
+          .fill(participantUserName);
+        await participantPage
+          .getByRole("button", { name: "セッションに参加" })
+          .click();
+
+        // 参加者ユーザー画面に投票開始ボタンが表示されないことを確認
+        await expect(
+          participantPage.getByRole("button", { name: "投票開始" }),
+        ).not.toBeVisible();
+        // ホストユーザー画面に投票開始ボタンが表示されることを確認
+        await expect(
+          hostPage.getByRole("button", { name: "投票開始" }),
+        ).toBeVisible();
+
+        // ホストが投票を開始する
+        await hostPage.getByRole("button", { name: "投票開始" }).click();
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票ボタンが表示されることを確認
-        // TODO ホストユーザー画面に投票ボタンが表示されることを確認
-        // TODO 参加者ユーザーが投票する
+        // 参加者ユーザー画面に投票ボタンが表示されることを確認
+        await expect(
+          participantPage.getByRole("button", { name: "0" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "1" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "2" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "3" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "5" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "8" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "13" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "20" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "40" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "100" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "∞" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "?" }),
+        ).toBeVisible();
+        // ホストユーザー画面に投票ボタンが表示されることを確認
+        await expect(hostPage.getByRole("button", { name: "0" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "1" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "2" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "3" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "5" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "8" })).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "13" }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "20" }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "40" }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "100" }),
+        ).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "∞" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "?" })).toBeVisible();
+
+        // 参加者ユーザーが投票する
+        await participantPage.getByRole("button", { name: "5" }).click();
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票状況が表示されることを確認
-        // TODO ホストユーザー画面に投票状況が表示されることを確認
-        // TODO 参加者ユーザーが投票内容を変更する
+        // 参加者ユーザー画面に投票状況が表示されることを確認
+        await expect(
+          participantPage.getByText("投票が完了しました"),
+        ).toBeVisible();
+        // ホストユーザー画面に投票状況が表示されることを確認
+        await expect(hostPage.getByText("1名が投票しました")).toBeVisible();
+
+        // 参加者ユーザーが投票内容を変更する
+        await participantPage.getByRole("button", { name: "8" }).click();
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票状況が表示されることを確認
-        // TODO ホストユーザー画面に投票状況が表示されることを確認
-        // TODO ホストユーザーが投票する
+        // 参加者ユーザー画面に投票状況が表示されることを確認
+        await expect(
+          participantPage.getByText("投票が完了しました"),
+        ).toBeVisible();
+        // ホストユーザー画面に投票状況が表示されることを確認
+        await expect(hostPage.getByText("1名が投票しました")).toBeVisible();
+
+        // ホストユーザーが投票する
+        await hostPage.getByRole("button", { name: "13" }).click();
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票状況が表示されることを確認
-        // TODO ホストユーザー画面に投票状況が表示されることを確認
-        // TODO ホストが投票を公開する
+        // 参加者ユーザー画面に投票状況が表示されることを確認
+        await expect(
+          participantPage.getByText("投票が完了しました"),
+        ).toBeVisible();
+        // ホストユーザー画面に投票状況が表示されることを確認
+        await expect(hostPage.getByText("2名が投票しました")).toBeVisible();
+
+        // ホストが投票を公開する
+        await hostPage.getByRole("button", { name: "投票を公開" }).click();
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票開始ボタンが表示されないことを確認
-        // TODO ホストユーザー画面に投票開始ボタンが表示されないことを確認
-        // TODO ホストが投票を開始する
+        // 参加者ユーザー画面に投票開始ボタンが表示されないことを確認
+        await expect(
+          participantPage.getByRole("button", { name: "投票開始" }),
+        ).not.toBeVisible();
+        // ホストユーザー画面に投票開始ボタンが表示されないことを確認
+        await expect(
+          hostPage.getByRole("button", { name: "投票開始" }),
+        ).not.toBeVisible();
+
+        // ホストが投票を公開する
+        await hostPage.getByRole("button", { name: "投票を公開" }).click();
+
+        // ホストが投票を開始する
+        await hostPage.getByRole("button", { name: "投票開始" }).click();
+
         // 画面表示確認
-        // TODO 参加者ユーザー画面に投票ボタンが表示されることを確認
-        // TODO ホストユーザー画面に投票状況が表示されることを確認
-        // TODO ホストユーザー画面に投票ボタンが表示されることを確認
-        // TODO ホストユーザー画面に投票状況が表示されることを確認
+        // 参加者ユーザー画面に投票ボタンが表示されることを確認
+        await expect(
+          participantPage.getByRole("button", { name: "0" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "1" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "2" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "3" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "5" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "8" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "13" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "20" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "40" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "100" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "∞" }),
+        ).toBeVisible();
+        await expect(
+          participantPage.getByRole("button", { name: "?" }),
+        ).toBeVisible();
+        // ホストユーザー画面に投票状況が表示されることを確認
+        await expect(hostPage.getByText("2名が投票しました")).toBeVisible();
+        // ホストユーザー画面に投票ボタンが表示されることを確認
+        await expect(hostPage.getByRole("button", { name: "0" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "1" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "2" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "3" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "5" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "8" })).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "13" }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "20" }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "40" }),
+        ).toBeVisible();
+        await expect(
+          hostPage.getByRole("button", { name: "100" }),
+        ).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "∞" })).toBeVisible();
+        await expect(hostPage.getByRole("button", { name: "?" })).toBeVisible();
       });
     });
 
     test.describe("Tシャツサイズ", () => {
-      test("投票開始→投票→投票公開→投票開始", async () => {
-        // TODO フィボナッチと同様のテストを実施
-      });
+      // TODO 実装
     });
 
     test.describe("2の累乗", () => {
-      test("投票開始→投票→投票公開→投票開始", async () => {
-        // TODO フィボナッチと同様のテストを実施
-      });
+      // TODO 実装
     });
   });
 });
