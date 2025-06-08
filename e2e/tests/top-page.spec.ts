191eaffa46619d573cde0eaca7e96351048ca19e
canpok1 <6isstrong@gmail.com>
2025-06-08 05:15:08 +0000

    トップページのe2eテストを充実化

--- e2e/tests/top-page.spec.ts
+++ e2e/tests/top-page.spec.ts
@@ -1,8 +1,27 @@
-
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('/');
+});
 
+test('タイトルが設定されていること', async ({ page }) => {
     await expect(page).toHaveTitle(/Web Toolbox/);
+});
+
+test('プランニングポーカーへのリンクが存在すること', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'プランニングポーカー' })).toBeVisible();
+});
+
+test('トークルーレットへのリンクが存在すること', async ({ page }) => {
+    await expect(page.getByRole('link', { name: 'トークルーレット' })).toBeVisible();
+});
+
+test('プランニングポーカーへのリンクをクリックできること', async ({ page }) => {
+    await page.getByRole('link', { name: 'プランニングポーカー' }).click();
+    await expect(page).toHaveURL(/planning-poker/);
+});
+
+test('トークルーレットへのリンクをクリックできること', async ({ page }) => {
+    await page.getByRole('link', { name: 'トークルーレット' }).click();
+    await expect(page).toHaveURL(/talk-roulette/);
 });
