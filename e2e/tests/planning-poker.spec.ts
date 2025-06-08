6a39bb0fce9c872f5cc12e3cfc89b9078a0bcfd9
canpok1 <6isstrong@gmail.com>
2025-06-08 05:27:24 +0000

    プランニングポーカーのトップページのe2eテストを追加

--- e2e/tests/planning-poker.spec.ts
+++ e2e/tests/planning-poker.spec.ts
@@ -1,9 +1,20 @@
-
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-    await page.goto('/planning-poker');
+test.describe('トップページ', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/planning-poker');
+    });
 
-    await expect(page).toHaveTitle(/プランニングポーカー/);
-    await expect(page).toHaveTitle(/Web Toolbox/);
+    test('タイトルがあること', async ({ page }) => {
+        await expect(page).toHaveTitle(/プランニングポーカー/);
+        await expect(page).toHaveTitle(/Web Toolbox/);
+    });
+
+    test('「セッションを作成」ボタンのリンク先が/planning-poker/sessions/createであること', async ({ page }) => {
+        await expect(page.getByRole('link', { name: 'セッションを作成' })).toHaveAttribute('href', '/planning-poker/sessions/create');
+    });
+
+    test('「セッションに参加」ボタンのリンク先が/planning-poker/sessions/joinであること', async ({ page }) => {
+        await expect(page.getByRole('link', { name: 'セッションに参加' })).toHaveAttribute('href', '/planning-poker/sessions/join');
+    });
 });
