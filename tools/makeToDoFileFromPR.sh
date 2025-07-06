d39ed7baeb1819e8a67316b2fc79eb7d48e492cd
canpok1 <6isstrong@gmail.com>
2025-07-06 08:23:17 +0000

    スクリプトを微調整

--- tools/makeToDoFileFromPR.sh
+++ tools/makeToDoFileFromPR.sh
@@ -9,7 +9,16 @@ fi
 
 cd "$(dirname "$0")/.."
 
-prompt="PR ${1} のレビューコメントをタスク化して tmp/todo 配下に1ファイル1タスクで保存して。なおファイルのフォーマットは docs/templates/todo.md に従うこと。"
-echo "prompt >>> $prompt"
+prompt=$(cat << EOS
+プルリクエスト #${1} のレビューコメントをタスク化し tmp/todo 配下に1ファイル1タスクで保存して。
+- ファイルフォーマットは docs/templates/todo.md に従うこと。
+- レビューコメントの取得はgithub MCPを利用すること。
+- リポジトリの情報は git remote -v でで確認すること。
+- 解決済みのレビューコメントはタスク化しないこと。
+EOS
+)
+echo "prompt >>>"
+echo "$prompt"
 
-gemini -y -p "$prompt"
+echo "run gemini >>>"
+gemini -y -m "gemini-2.5-flash" -p "$prompt"
