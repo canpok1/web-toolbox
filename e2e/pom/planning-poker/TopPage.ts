f5a6889cd45b446b195edf53e74804bdfac48a62
canpok1 <canpok1@users.noreply.github.com>
2025-07-06 18:34:47 +0900

    refactor(e2e): TopPage.spec.ts (Planning Poker) を POM を使用するようにリファクタリング (#109) (#112)
    * refactor(e2e): TopPage.spec.ts (Planning Poker) を POM を使用するようにリファクタリング (#109)
    
    e2e/tests/planning-poker/TopPage.spec.ts を Playwright の Page Object Model (POM) パターンに準拠するようにリファクタリングしました。
    
    - PlanningPokerTopPagePom の新規作成とインポート
    - test.beforeEach ブロックで PlanningPokerTopPagePom インスタンスを初期化
    - Playwright セレクタを適切な PlanningPokerTopPagePom のプロパティとメソッドに置き換え
    
    * refactor(e2e): TopPage.ts の page プロパティに readonly 修飾子を追加
    
    * refactor(e2e): TopPage.spec.ts を POM パターンに沿って修正

---
+++ e2e/pom/planning-poker/TopPage.ts
@@ -0,0 +1,29 @@
+import { expect, type Page } from "@playwright/test";
+
+export class PlanningPokerTopPagePom {
+  constructor(private readonly page: Page) {}
+
+  async goto() {
+    await this.page.goto("/planning-poker");
+  }
+
+  async expectTitleToContain(text: string) {
+    await expect(this.page).toHaveTitle(new RegExp(text));
+  }
+
+  get createSessionLink() {
+    return this.page.getByRole("link", { name: "セッションを作成" });
+  }
+
+  get joinSessionLink() {
+    return this.page.getByRole("link", { name: "セッションに参加" });
+  }
+
+  async clickCreateSessionLink() {
+    await this.createSessionLink.click();
+  }
+
+  async clickJoinSessionLink() {
+    await this.joinSessionLink.click();
+  }
+}
--- e2e/tests/planning-poker/TopPage.spec.ts
+++ e2e/tests/planning-poker/TopPage.spec.ts
@@ -1,28 +1,29 @@
 import { expect, test } from "@playwright/test";
+import { PlanningPokerTopPagePom } from "../../pom/planning-poker/TopPage";
 
 test.describe("トップページ", () => {
+  let planningPokerTopPage: PlanningPokerTopPagePom;
   test.beforeEach(async ({ page }) => {
-    await page.goto("/planning-poker");
+    planningPokerTopPage = new PlanningPokerTopPagePom(page);
+    await planningPokerTopPage.goto();
   });
 
-  test("タイトルがあること", async ({ page }) => {
-    await expect(page).toHaveTitle(/プランニングポーカー/);
-    await expect(page).toHaveTitle(/Web Toolbox/);
+  test("タイトルがあること", async () => {
+    await planningPokerTopPage.expectTitleToContain("プランニングポーカー");
+    await planningPokerTopPage.expectTitleToContain("Web Toolbox");
   });
 
-  test("「セッションを作成」ボタンのリンク先が/planning-poker/sessions/createであること", async ({
-    page,
-  }) => {
-    await expect(
-      page.getByRole("link", { name: "セッションを作成" }),
-    ).toHaveAttribute("href", "/planning-poker/sessions/create");
+  test("「セッションを作成」ボタンのリンク先が/planning-poker/sessions/createであること", async () => {
+    await expect(planningPokerTopPage.createSessionLink).toHaveAttribute(
+      "href",
+      "/planning-poker/sessions/create",
+    );
   });
 
-  test("「セッションに参加」ボタンのリンク先が/planning-poker/sessions/joinであること", async ({
-    page,
-  }) => {
-    await expect(
-      page.getByRole("link", { name: "セッションに参加" }),
-    ).toHaveAttribute("href", "/planning-poker/sessions/join");
+  test("「セッションに参加」ボタンのリンク先が/planning-poker/sessions/joinであること", async () => {
+    await expect(planningPokerTopPage.joinSessionLink).toHaveAttribute(
+      "href",
+      "/planning-poker/sessions/join",
+    );
   });
 });
